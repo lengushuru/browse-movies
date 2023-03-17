cab52ea78932ef755f251fdee97b56e5bb370888
lengushuru charles <charleslengushuru@gmail.com>
2023-03-17 14:40:28 +0300

    Fix linters error

--- src/GetComment.js
+++ src/GetComment.js
@@ -1,22 +1,15 @@
-import {BASE_URL} from './apis';
+import { BASE_URL } from './apis.js';
 
-const getComment = async (itemId) =>{
-  
-  return fetch(BASE_URL+'/comments'+ '?item_id=' + itemId)
-  .then(response => {
+const getComment = async (itemId) => fetch(`${BASE_URL}/comments?item_id=${itemId}`)
+  .then((response) => {
     if (response.ok) {
       return response.json();
-    } else {
-      throw new Error('Error: ' + response.status);
     }
+    throw new Error(`Error: ${response.status}`);
   })
-  .then(data => {
-    return data;
-  })
-  .catch(error => {
-    console.error(error);
-  });
-
-}
+  .then((data) => data);
+  // .catch((error) => {
+  //   console.error(error);
+  // });
 
 export default getComment;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './styles.css';
-// import {BASE_URL} from './apis';
-import postcomment from './PostComment';
-import getComment from './GetComment';
+import postcomment from './PostComment.js';
+import getComment from './GetComment.js';
+import commentCounter from './Commentcount.js';
 
 const renderModalPopup = async (modalPopup, modalid) => {
   const comments = await getComment(modalid);
@@ -9,29 +9,30 @@ const renderModalPopup = async (modalPopup, modalid) => {
   if (comments && comments.length > 0) {
     commentsList = comments
       .map(
-        (comment) =>
-          `<li>${comment.creation_date} ${comment.username} ${comment.comment}</li>`
+        (comment) => `<li><div class='top-div'><p>${comment.creation_date}</p><p>${comment.username}</p></div><div class = 'commentdiv'>${comment.comment}</div></li>`,
       )
       .join('');
   } else {
     commentsList = '<li>No comments yet</li>';
   }
-
+  const count = commentCounter(comments);
   const modalPopupHTML = `
     <div class="modal-popup">
+
       <button class="close-modal">X</button>
       <div class="image">
         <img src=${modalPopup.image.original} alt="movie image">
       </div>
       <h2>${modalPopup.name}</h2>
       <p class="description">${modalPopup.summary}</p>
-      <ul class="comments">
+      <p class="rating">Total comments: ${count}</p>
+      <ul class="commentbox">
         ${commentsList}
       </ul>
       <form id="submit${modalid}">
         <input class="name" type="text" placeholder="Your name" required maxlength="50">
         <input class="comment" type="text" placeholder="Add a comment" required maxlength="200">
-        <button type="submit">Submit</button>
+        <button class = "submit" type="submit">Submit</button>
       </form>
     </div>
   `;
@@ -52,8 +53,7 @@ const renderModalPopup = async (modalPopup, modalid) => {
       const updatedComments = await getComment(modalid);
       const updatedCommentsList = updatedComments
         .map(
-          (comment) =>
-            `<li>${comment.creation_date} ${comment.username} ${comment.comment}</li>`
+          (comment) => `<li>${comment.creation_date} ${comment.username} ${comment.comment}</li>`,
         )
         .join('');
       const commentsListElement = form.previousElementSibling;
@@ -64,27 +64,20 @@ const renderModalPopup = async (modalPopup, modalid) => {
 
   document.querySelector('.close-modal').addEventListener('click', () => {
     document.querySelector('.popup-modal').style.display = 'none';
+    window.location.reload();
   });
 
   return modalPopupElement;
 };
 
-
 const fetchSingleShowComment = async (e) => {
-
-  const id  = e.target.id;
-  console.log(id)
-  
+  const { id } = e.target;
   const url = `https://api.tvmaze.com/shows/${id}`;
   const response = await fetch(url);
   const result = await response.json();
-  console.log(result);
-  let commentslist = await getComment(id); 
-console.log('result',commentslist)
-renderModalPopup(result,id)
-document.querySelector('.popup-modal').style.display = 'block';
+  // const commentslist = await getComment(id);
+  renderModalPopup(result, id);
+  document.querySelector('.popup-modal').style.display = 'block';
 };
 
-document.querySelectorAll('.comments').forEach(item =>item.addEventListener('click',  (e)=> fetchSingleShowComment(e))) 
-
-
+document.querySelectorAll('.comments').forEach((item) => item.addEventListener('click', (e) => fetchSingleShowComment(e)));
